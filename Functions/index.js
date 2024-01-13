dac91a19fbdfad944229928a3de15a954f94d37d
Karlo11111 <97040384+Karlo11111@users.noreply.github.com>
2024-01-13 13:23:35 +0100

    Changing some cloud functions
    Fixed sending emails as well as changed it so that it creates a collection with deleted reservations and stores them
    
    Co-Authored-By: Aschmenzy <128328179+Aschmenzy@users.noreply.github.com>

--- Functions/index.js
+++ Functions/index.js
@@ -1,6 +1,5 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-
 const {Resend} = require("resend");
 
 admin.initializeApp();
@@ -9,23 +8,30 @@ const resend = new Resend("re_Vecg5k6M_PvVBCWq2fbmahGcpFz4VFyrs");
 
 exports.sendCheckInEmails = functions
     .region("europe-west1")
-    .pubsub.schedule("every 24 hours") // Run daily
+    .pubsub.schedule("0 8 * * *")
+    .timeZone("Europe/Zagreb")
     .onRun(async (context) => {
       const db = admin.firestore();
 
+      const zadarTimeOffset = 1; // Zadar is currently UTC+1
+
       const todayStart = new Date();
-      todayStart.setUTCHours(0, 0, 0, 0); // Start of the day UTC
+      todayStart.setHours(todayStart.getHours() + zadarTimeOffset);
+      todayStart.setHours(0, 0, 0, 0); // Start of the day in Zadar time
 
       const todayEnd = new Date(todayStart);
-      todayEnd.setUTCDate(todayEnd.getUTCDate() + 1); // End of the day UTC
+      todayEnd.setDate(todayEnd.getDate() + 1); // End of the day in Zadar time
 
       console.log(`todayStart: ${todayStart.toISOString()}`);
       console.log(`todayEnd: ${todayEnd.toISOString()}`);
 
       const reservationsSnapshot = await db
           .collection("UsersReservation")
-          .where("checkInDate", ">=", admin.firestore
-              .Timestamp.fromDate(todayStart))
+          .where(
+              "checkInDate",
+              ">=",
+              admin.firestore.Timestamp.fromDate(todayStart),
+          )
           .where("checkInDate", "<", admin.firestore
               .Timestamp.fromDate(todayEnd))
           .get();
@@ -35,41 +41,68 @@ exports.sendCheckInEmails = functions
         return null;
       }
 
-      const tasks = reservationsSnapshot
-          .docs.map(async (doc) => {
-            const reservation = doc.data();
-            const email = reservation.email;
-            const password = reservation.password;
+      const tasks = reservationsSnapshot.docs.map(async (doc) => {
+        const reservation = doc.data();
+        const email = reservation.email;
+        const firstName = reservation.firstName;
+        const password = reservation.password;
 
-            try {
-              const userRecord = await admin.auth()
-                  .createUser({email, password});
-              console.log("Successfully created new user:", userRecord.uid);
-
-              const emailContent = `...`; // Your email content here
-
-              await resend.emails.send({
-                from: "onboarding@resend.dev",
-                to: email,
-                subject: "Your Aedis Check-In Account Information",
-                html: emailContent,
-              });
-
-              console.log("Email sent to:", email);
-            } catch (error) {
-              console.error("Error in operation for:", email, error);
+        try {
+        // Attempt to create a user
+          try {
+            await admin.auth().createUser({email, password});
+            console.log("Successfully created new user:", email);
+          } catch (error) {
+            if (error.code ===
+              "auth/email-already-exists") {
+              console.log("User already exists with email:"
+                  , email);
+            } else {
+              throw error;
             }
+          }
+
+          // Email content
+          const emailContent = `
+          <p>Hello ${firstName},</p>
+          <p>Welcome to Aedis! Here are 
+          your login credentials for our 
+          online check-in system:</p>
+          <p><b>Email:</b> ${email}</p>
+          <p><b>Password:</b> ${password}</p>
+          <p>We recommend changing your password 
+          after your first login for security reasons. 
+          If you have any questions or need assistance, 
+          please feel free to contact us.</p>
+          <p>Looking forward to welcoming you,</p>
+          <p>The Aedis Team</p>
+        `;
+
+          // Send email regardless of user creation status
+          await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: email,
+            subject: "Your Aedis Check-In Account Information",
+            html: emailContent,
           });
+
+          console.log("Email sent to:", email);
+        } catch (error) {
+          console.error("Error in operation for:", email, error);
+        }
+      });
       await Promise.all(tasks);
     });
 
-exports.deletePastReservations = functions
-    .pubsub.schedule("0 22 * * *")
+exports.deletePastReservations = functions.pubsub
+    .schedule("0 22 * * *")
     .timeZone("Europe/Zagreb") // Specify Zadar time zone
     .onRun(async (context) => {
       const now = admin.firestore.Timestamp.now();
-      const reservationsRef = admin.firestore()
-          .collection("UsersReservation");
+      const reservationsRef = admin.firestore().collection("UsersReservation");
+      const deletedReservationsRef = admin.firestore()
+          .collection("DeletedReservations");
+
       const snapshot = await reservationsRef
           .where("checkInDate", "<", now).get();
       if (snapshot.empty) {
@@ -78,7 +111,14 @@ exports.deletePastReservations = functions
       }
 
       const batch = admin.firestore().batch();
-      snapshot.docs.forEach((doc) => batch.delete(doc.ref));
+
+      snapshot.docs.forEach((doc) => {
+        // Add the reservation to the DeletedReservations collection
+        const deletedReservationData = {...doc.data(), deletedAt: now};
+        deletedReservationsRef.doc(doc.id).set(deletedReservationData);
+        //delete the reservation from the reservation collection
+        batch.delete(doc.ref);
+      });
 
       await batch.commit();
       console.log("Past reservations deleted.");
